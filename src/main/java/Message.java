22ff446c8e705af4cc3b374ee707283653e60e7b
JuliansRecent <juliansrecent@gmail.com>
2023-01-18 05:45:56 -0800

    Module complete. Prompt the user for an input, assign it to memory, then print the user's input.

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -6,8 +6,13 @@ public class Message {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Print "Write a message"
         System.out.println("Write a message:");
-        // Write your program here
+        // Read the string written by the user, and assign it to program memory as the variable "message"
+        String message = scanner.nextLine();
+        // Print the message written by the user
+        System.out.println(message);
+
 
     }
 }
